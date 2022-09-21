8c35797741c55e8dfd888159fdda4a9615a21d59
Scolar02 <njugunascolar83@gmail.com>
2022-09-21 00:19:18 +0000

    fixing typing error in output

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
-for i in range(0, 10):
-    for j in range((i+1), 10):
-        if (i is not 8) or (j is not 9):
-            print("{}{}, ".format(i, j), end="")
-        else:
-            print("{}{}".format(i, j))
+#!/usr/bin/python
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}".format(i, j), end=', ')
+print("{:d}{:d}".format(i + 1, j))
