fc097ad429b7c71f117497b9bfa97414c448f6cd
Scolar02 <njugunascolar83@gmail.com>
2022-10-17 12:37:43 +0000

    fixing errors

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-Module 3-rectangle
+Module 2-rectangle
 Contains class Rectangle with private attribute width and height,
-public area and perimeter methods, and allows printing #'s
+and public area and perimeter methods
 """
 
 
@@ -22,7 +22,6 @@ class Rectangle:
         height(self, value)
         area(self)
         perimeter(self)
-        __str__(self)
     """
     def __init__(self, width=0, height=0):
         """ Initialize rectangles """
@@ -42,7 +41,6 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
-
 
     @property
     def height(self):
@@ -67,10 +65,3 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return (2 * self.__width) + (2 * self.height)
-
-    def __str__(self):
-        """ Prints rectangle with #'s """
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        pic = "\n".join(["#" * self.__width for rows in range(self.__height)])
-        return pic
