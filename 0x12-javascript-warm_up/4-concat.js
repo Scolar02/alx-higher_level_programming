76a4d5e55a8e41b6a46b0f192c68e4e2913d7c09
Scolar02 <njugunascolar83@gmail.com>
2023-06-15 08:08:22 -0700

    tsk 4

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 
-console.log(`&{process.argv[2]} is &{process.argv[3]}`);
+#!/usr/bin/env node
+
+const arg1 = process.argv[2] || 'undefined';
+const arg2 = process.argv[3] || 'undefined';
+
+console.log(`${arg1} is ${arg2}`);
