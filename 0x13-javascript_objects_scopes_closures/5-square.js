0a59a0818b3f1e836d331667530f5c490eb01720
Scolar02 <njugunascolar83@gmail.com>
2023-06-16 05:47:15 -0700

    square inherits properties from a rectangle

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,26 +1,11 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
-  constructor (w, h) {
-    this.width = w;
-    this.height = h;
-  }
-};
+const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor (size) {
+  constructor(size) {
     super(size, size);
   }
-
-  print () {
-    const row = 'X'.repeat(this.width);
-    for (let i = 0; i < this.height; i++) {
-      console.log(row);
-    }
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
 }
+
+module.exports = Square;
