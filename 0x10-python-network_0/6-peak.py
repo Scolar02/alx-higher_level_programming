fcfa04ab5f8d4febcafaa94cf39097d71f32b0b2
Scolar02 <njugunascolar83@gmail.com>
2023-06-30 06:18:59 -0700

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-Function that finds a peak in a list of unsorted integers
+finds a peak in a list of unsorted integers
 """
 
 
 def find_peak(list_of_integers):
     """
-    finds num that's greater than both left and right
+    num greater than both left and right
     """
     if len(list_of_integers) == 0:
         return None
