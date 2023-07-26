6fd64113b08e58735370d2dcfe0941a53e883973
Scolar02 <njugunascolar83@gmail.com>
2023-07-26 07:32:37 -0700

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,14 @@
 #!/usr/bin/node
 
-let filename = process.argv[2];
 const fs = require('fs');
+
+if (process.argv.length < 3) {
+  console.log('Usage: node script_name.js <file_path>');
+  process.exit(1);
+}
+
+let filename = process.argv[2];
+
 fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
     console.log(err);
